9f2ea428a04ea461287bf31e90ddeb29529e3375
Bill Rivers <learn@williamrivers.com>
2018-09-23 11:22:31 -0700

    Chapter1 with all tests passing and appveyor happy (#1)
    * tests all complete locally
    
    * Needs to be VS 2017 for Appveyor
    
    * Path mistake
    
    * Fixing appveyor nuget issue
    
    * Making build less chatty

---
+++ Chapter1/VideoStoreSupport.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Chapter1
+{
+    public enum PayType : byte
+    {
+        Tragedy,
+        Comedy
+    }
+
+    public class Play
+    {
+        public readonly string Name;
+        public readonly PayType PayType;
+
+        public Play(string name, PayType payType)
+        {
+            Name = name;
+            PayType = payType;
+        }
+    }
+
+    public class Performance
+    {
+        public readonly int Audience;
+        public readonly string PlayId;
+
+        public Performance(string playId, int audience)
+        {
+            PlayId = playId;
+            Audience = audience;
+        }
+    }
+
+    public class Invoice
+    {
+        public readonly string Customer;
+        public readonly ImmutableList<Performance> Performances;
+
+        public Invoice(string customer, IEnumerable<Performance> performances)
+        {
+            Customer = customer;
+            Performances = performances.ToImmutableList();
+        }
+    }
+
+    public interface IVideoStore
+    {
+        string Statement(Invoice invoice, IReadOnlyDictionary<string, Play> plays);
+    }
+}
